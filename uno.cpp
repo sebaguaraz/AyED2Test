10671897c364f0120ce820fe48dfccf3b97aa3e8
sebaguaraz <101686077+sebaguaraz@users.noreply.github.com>
2023-04-16 13:00:23 -0300

    Update uno.cpp

--- uno.cpp
+++ uno.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World! Algoritmo y estructuras de datos 2" << std::endl;
     return 0;
 }
